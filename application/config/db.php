c3bbf262a64cf61d717cdc7c81a84f6edf54b140
guseynovagid <guseynowww@inbox.ru>
2023-01-22 16:48:40 +0300

    Add project to GitHub

---
+++ application/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost',
+	'name' => 'mvc-blog',
+	'user' => 'root',
+	'password' => '',
+];
---
+++ application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/me.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Я программист</h1>
+                    <span class="subheading">ом хочу стать</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Здесь могло быть больше текста, но мне лень писать :)</p>
+        </div>
+    </div>
+</div>
